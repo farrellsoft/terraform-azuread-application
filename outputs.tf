aa6b781db66340e84832f75ac59960edd3ccd135
Jason Farrell <imjason@gmail.com>
2023-01-13 08:44:20 -0500

    added application id uri to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output application_id {
   value       = azuread_application.this.application_id
   description = "The application ID of the backend application."
 }
+
+output application_id_uri {
+  value       = "api://${var.application_name}"
+  description = "The application ID URI of the backend application."
+}
